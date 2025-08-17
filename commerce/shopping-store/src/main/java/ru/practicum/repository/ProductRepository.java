fad9d3022e5b8dbc2de99a9b1c16c5ba1e83083b
Valerii_Butko <lb2005lba@gmail.com>
2025-08-17 17:10:41 +0500

    поменяла импорт

--- commerce/shopping-store/src/main/java/ru/practicum/repository/ProductRepository.java
+++ commerce/shopping-store/src/main/java/ru/practicum/repository/ProductRepository.java
@@ -3,7 +3,7 @@ package ru.practicum.repository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.enums.store.ProductCategory;
-import ru.practicum.enums.ProductState;
+import ru.practicum.enums.store.ProductState;
 import ru.practicum.model.Product;
 
 import java.util.List;
